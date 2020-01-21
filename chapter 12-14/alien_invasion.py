ea414f071950c98314c483cbfa6d1f946e2564fb
sunstriderLHT <55929874+sunstriderLHT@users.noreply.github.com>
2020-01-21 11:09:44 -0500

    1.21 (#1)

--- chapter 12-14/alien_invasion.py
+++ chapter 12-14/alien_invasion.py
@@ -12,6 +12,8 @@ from ship import Ship
 import game_function as gf
 from pygame.sprite import Group
 from game_stats import GameStats
+from button import Button
+from scoreboard import Scoreboard
 
 def run_game():
     #初始化背景设置并创建一个指定大小的显示窗口
@@ -20,8 +22,14 @@ def run_game():
     ai_settings = Settings()
     #创建一个指定大小的窗口
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
-    #窗口的名称
+    #窗口的名称    
     pygame.display.set_caption("Alien Invation")
+    
+    play_button = Button(ai_settings,screen,"Play")
+    #创建储存游戏统计信息的实例，并创建记分牌
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings,screen, stats)
+    
     #创建一个Ship实例
     ship = Ship(ai_settings,screen)
     #创建一个bullets群组
@@ -31,18 +39,19 @@ def run_game():
     #创建外星人
     gf.create_fleet(ai_settings,screen,ship,aliens)
     
-    stats = GameStats(ai_settings)
+    
     
     
     while True:
+        gf.check_events(ai_settings, screen, stats,sb, play_button, ship,aliens, bullets)
         
-        gf.check_events(ai_settings,screen,ship,bullets)      
-        ship.update()    
-        gf.check_bullet_alien_collisions(ai_settings,screen,ship,aliens,bullets)
-        gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
+        if stats.game_active:
+            ship.update()    
+            gf.check_bullet_alien_collisions(ai_settings,screen,stats,sb,ship,aliens,bullets)
+            gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
+            gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
         
-        gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
-        gf.update_screen(ai_settings,screen,ship,aliens,bullets) 
+        gf.update_screen(ai_settings,screen,stats,sb,ship,aliens,bullets,play_button) 
         
         
 run_game() 
